8e6efbe7f2852d2800d9c394881e04068d53072e
Khoi Nguyen Tan <khoi.nt.21@gmail.com>
2020-10-04 17:55:39 +0700

    Complete Upload Action

--- Controllers/AlbumController.cs
+++ Controllers/AlbumController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AlbumAPIv2.Services;
 using AlbumAPIv2.Models;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AlbumAPIv2.Controllers
 {
@@ -14,13 +16,16 @@ namespace AlbumAPIv2.Controllers
     public class AlbumController : ControllerBase
     {
         private readonly AlbumService _albumService;
-        public AlbumController()
+        private IHostingEnvironment _enviroment;
+        public AlbumController(IHostingEnvironment env)
         {
             _albumService = new AlbumService();
+            _enviroment = env;
         }
         [HttpGet]
         public IEnumerable<string> Index()
         {
+
             var res = new List<string>();
             res.AddRange(_albumService.GetAlbum("Landscape"));
             res.AddRange(_albumService.GetAlbum("People"));
@@ -44,6 +49,27 @@ namespace AlbumAPIv2.Controllers
         public IEnumerable<string> TechList()
         {
             return _albumService.GetAlbum("Tech");
+        }
+
+        [HttpPost("upload")]
+        public string OnPostUpload([FromForm(Name = "file")] IFormFile file, [FromForm] string subDir)
+        {
+            string s;
+            if (file != null || file.Length != 0)
+            {
+                using (FileStream fs =
+                    new FileStream((_enviroment.WebRootPath + "/" + subDir + "/" + file.FileName + ".jpg"), FileMode.Create))
+                {
+                    file.CopyToAsync(fs);
+
+                }
+                s = "OK";
+            }
+            else
+            {
+                s = "Not OK";
+            }
+            return s;
         }
     }
 }
